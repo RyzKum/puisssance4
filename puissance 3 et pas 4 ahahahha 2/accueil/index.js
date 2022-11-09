481e9d4b8b223ec8185fa060214aa50cfb801c0b
BaptisteGrimaldi <grimaldi.baptiste@gmail.com>
2022-11-09 16:04:18 +0100

    Implémentation du chat

---
+++ puissance 3 et pas 4 ahahahha 2/accueil/index.js
@@ -0,0 +1,15 @@
+let hautTriangle = document.getElementById("triangleChat");
+let basTriangle = document.getElementById("hautDuTriangle");
+let iconeChat = document.getElementById("pChat");
+
+iconeChat.addEventListener("click",()=>{
+    console.log("yes");
+    if(hautTriangle.style.visibility == "hidden"){
+        hautTriangle.style.visibility = "visible";
+        basTriangle.style.visibility = "visible";
+    }
+    else{
+        hautTriangle.style.visibility = "hidden";
+        basTriangle.style.visibility = "hidden";
+    }
+})
