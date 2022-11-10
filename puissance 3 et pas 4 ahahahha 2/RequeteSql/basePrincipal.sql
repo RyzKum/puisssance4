5ce11317fa48b4ecb0c0dda3e6cf2156b845a5d2
BaptisteGrimaldi <grimaldi.baptiste@gmail.com>
2022-11-10 10:55:52 +0100

    fichier requete

--- puissance 3 et pas 4 ahahahha 2/RequeteSql/basePrincipal.sql
+++ puissance 3 et pas 4 ahahahha 2/RequeteSql/basePrincipal.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `base_test_p4`
+-- Base de données : `basePrincipal`
 --
 
 -- --------------------------------------------------------
