4a787cf238a663e6be7ce8123a96478372223844
Sambachpx <samba.champroux@edu.esiee-it.fr>
2022-11-10 15:31:05 +0100

    requettes sql

--- puissance 3 et pas 4 ahahahha 2/RequeteSql/requetesql.sql
+++ puissance 3 et pas 4 ahahahha 2/RequeteSql/requetesql.sql
@@ -1,21 +1,22 @@
--- /inscription d'un utilisateur/
-INSERT INTO Utlisateur (identifiant, email, mot_de_passe, pseudo, date_heure_inscription, date_heure_dern_co)
+/*inscription d'un utilisateur*/
+INSERT INTO `Utlisateur` (`identifiant`, `email`, `mot_de_passe`, `pseudo`, `date_heure_inscription`, `date_heure_dern_co`)
  VALUES ('3', 'test@test@gmail.com', 'azerty', 'sambatest', '2022-11-10 08:18:35.000000', '2022-11-10 08:18:35.000000');
 
 
--- /modif de email/
+/*modif de email*/
 
-UPDATE Utlisateur 
-SET email = 'samba.champroux2@gmail.com' 
-WHERE Utlisateur.identifiant = 1;
+UPDATE `Utlisateur` 
+SET `email` = 'samba.champroux2@gmail.com' 
+WHERE `Utlisateur`.`identifiant` = 1;
 
--- /modif de mdp/
-UPDATE Utlisateur 
-SET mot_de_passe = 'azertyfff' 
-WHERE Utlisateur.identifiant = 1;
+/*modif de mdp*/
+UPDATE `Utlisateur` 
+SET `mot_de_passe` = 'azertyfff' 
+WHERE `Utlisateur`.`identifiant` = 1;
 
-INSERT INTO jeu (id, nom_jeu) VALUES ('4', 'The Tower of memory');
 
+/* ajouter jeu à la base de données*/
+INSERT INTO `jeu` (`id`, `nom_jeu`) VALUES ('4', 'The Tower of memory');
 
 -- Requete Connexion
 
