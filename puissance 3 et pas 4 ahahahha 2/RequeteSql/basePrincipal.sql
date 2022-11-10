91d7b10bad56fe18d2bb99d889871046503c0bed
BaptisteGrimaldi <grimaldi.baptiste@gmail.com>
2022-11-10 09:21:44 +0100

    Base principal regroupant les jointures

---
+++ puissance 3 et pas 4 ahahahha 2/RequeteSql/basePrincipal.sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le : mer. 09 nov. 2022 à 09:45
+-- Version du serveur : 5.7.34
+-- Version de PHP : 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `base_test_p4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `jeu`
+--
+
+CREATE TABLE `jeu` (
+  `id` int(11) NOT NULL,
+  `nom_jeu` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message(tchat)`
+--
+
+CREATE TABLE `message(tchat)` (
+  `id` int(11) NOT NULL,
+  `id_jeu` int(11) NOT NULL,
+  `id_expediteur` int(11) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `date_heure_mess` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `score`
+--
+
+CREATE TABLE `score` (
+  `id` int(11) NOT NULL,
+  `id_joueur` int(255) NOT NULL,
+  `id_jeu` int(255) NOT NULL,
+  `difficulté_partie` int(3) NOT NULL,
+  `score_partie` int(11) NOT NULL,
+  `date_heure_partie` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Utlisateur`
+--
+
+CREATE TABLE `Utlisateur` (
+  `identifiant` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mot_de_passe` varchar(255) NOT NULL,
+  `pseudo` varchar(255) NOT NULL,
+  `date_heure_inscription` datetime NOT NULL,
+  `date_heure_dern_co` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `jeu`
+--
+ALTER TABLE `jeu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `message(tchat)`
+--
+ALTER TABLE `message(tchat)`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `score`
+--
+ALTER TABLE `score`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `Utlisateur`
+--
+ALTER TABLE `Utlisateur`
+  ADD PRIMARY KEY (`identifiant`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `jeu`
+--
+ALTER TABLE `jeu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `message(tchat)`
+--
+ALTER TABLE `message(tchat)`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `score`
+--
+ALTER TABLE `score`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `Utlisateur`
+--
+ALTER TABLE `Utlisateur`
+  MODIFY `identifiant` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
