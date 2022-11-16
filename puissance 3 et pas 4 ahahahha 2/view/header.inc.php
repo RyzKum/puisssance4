2b64f8ffb846a646e97be2e217ebaf43a343d62b
Sambachpx <samba.champroux@edu.esiee-it.fr>
2022-11-16 16:37:30 +0100

    foooter

--- puissance 3 et pas 4 ahahahha 2/view/header.inc.php
+++ puissance 3 et pas 4 ahahahha 2/view/header.inc.php
@@ -14,6 +14,7 @@
             <a class="headerBtn" href="../inscription/index.php">S'INSCRIRE</a>
             <a class="headerBtn" href="../login/index.php">SCORES</a>
             <a class="headerBtn" href="../puissance 3 et pas 4 ahahahha 2/contact/contact.php">CONTACTER</a>
+            <a class="headerBtn" href="../login/index.php">SCORES</a>
         </div>  
  </nav>
  
