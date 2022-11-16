3c7733cc5bcef43f07ab2dd0b32ee713448fa63b
Sambachpx <samba.champroux@edu.esiee-it.fr>
2022-11-16 16:14:47 +0100

    footer

--- puissance 3 et pas 4 ahahahha 2/contact/contact.php
+++ puissance 3 et pas 4 ahahahha 2/contact/contact.php
@@ -9,21 +9,9 @@
     <link rel="stylesheet" href="contact.css">
 </head>
 <body>
-    
-    <nav>
-        <div id="nav1">
-            <p>The Power Of Memory</p>
-         </div>
-    
-         <div id="ajunav2">
-            <div id="nav2">
-                <a class="headerBtn" href="../accueil/index.php">ACCUEIL</a>
-                <a class="headerBtn" href="../inscription/index.php">S'INSCRIRE</a>
-                <a class="headerBtn" href="../login/index.php">SCORES</a>
-                <a class="headerBtn" href="../../puissance 3 et pas 4 ahahahha 2/contact/contact.php">CONTACTER</a>
-            </div>
-         </div>
-    </nav>
+<?php
+    require "/Applications/MAMP/htdocs/github/puisssance4/puissance 3 et pas 4 ahahahha 2/view/header.inc.php";
+    ?>
     <header>
         <div class="image1">
         <p>NOUS CONTACTER</p>
@@ -64,74 +52,8 @@
             
         </div>
     </main>
-    <section class="ses5">
-        <footer>
-            <div id="footerPart1">
-                <div id="info">
-                    <p>Information</p>
-                    <p>Quisque commodo facilisis purus, interdum volutpat arcu viverra sed</p>
-                    <p><span class="infoOrange">Tel : </span><span class="tex"> 05 04 03 02</p>
-                    <p><span class="infoOrange">Email :</span> support@powerofmemory.com</p>
-                    <p><span class="infoOrange">Location : </span>Paris</p>
-                    <div id="reseau">
-                        <div class="logoOrange"><img src="image/logofacebook.jpeg" class="tailleImage"></img></div>
-                        <div class="logoOrange"><img src="image/twit.jpeg" class="tailleImage"></img></div>
-                        <div class="logoOrange"><img src="image/Google-Logo.png" class="tailleImage"></img></div>
-                        <div class="logoOrange"><img src="image/pint.png" class="tailleImage"></img></div>
-                        <div class="logoOrange"><img src="image/Instagram_icon.png.webp" class="tailleImage"></img></div>
-                    </div>
-                </div>
-    
-                <div id="power">
-                    <p>Power Of Memory</p>
-                    <p><span class="pointOrange">▪</span> <a class="loot" href="../login/index.php">Jouer ! </a> </p>
-                    <p><span class="pointOrange">▪</span> <a class="loot" href="../login/index.php">Les scores</a></p>
-                    <p><span class="pointOrange">▪</span> <a class="loot" href="../../puissance 3 et pas 4 ahahahha 2/contact/contact.php">Nous contacter </a></p>
-    
-                </div>
-            </div>
-    
-            <div id="footerPart2">
-                <!-- <div id="flecheOrange">
-                    <p>^</p>
-                </div> -->
-                <p id="copyRight">Copyright © 2022 Tous droits réservés</p>
-            </div>
-        </footer>
-    </section>
-    <!-- <footer>
-        <div id="footerPart1">
-            <div id="info">
-                <p>Information</p>
-                <p>Quisque commodo facilisis purus, interdum volutpat arcu viverra sed</p>
-                <p><span class="infoOrange">Tel :</span>06 05 04 03 02</p>
-                <p><span class="infoOrange">Email :</span> support@powerofmemory.com</p>
-                <p><span class="infoOrange">Location :</span>Paris</p>
-                <div id="reseau">
-                    <div class="logoOrange"><img src="image/Le-logo-Facebook.jpg" class="tailleImage"></img></div>
-                    <div class="logoOrange"><img src="image/twitter-logo.jpg" class="tailleImage"></img></div>
-                    <div class="logoOrange"><img src="image/Google-Logo.png" class="tailleImage"></img></div>
-                    <div class="logoOrange"><img src="image/Logo-Pinterest-1.jpg" class="tailleImage"></img></div>
-                    <div class="logoOrange"><img src="image/Instagram_icon.png.webp" class="tailleImage"></img></div>
-                </div>
-            </div>
-    
-            <div id="power">
-                <p>Power Of Memory</p>
-                <p><span class="pointOrange">▪</span>  Jouer !</p>
-                <p><span class="pointOrange">▪</span>  Les scores</p>
-                <p><span class="pointOrange">▪</span>  Nous contacter</p>
-                
-            </div>
-        </div>
-        
-        <div id="footerPart2">
-            <div id="flecheOrange">
-                <p>^</p>
-            </div>
-            <p id="copyRight">Copyright © 2022 Tous droits réservés</p>
-        </div>
-    </footer> -->
-    <a class="gotopbtn" href="#"><i class="fa-solid fa-angle-up"></i></a>
+    <?php
+    require "/Applications/MAMP/htdocs/github/puisssance4/puissance 3 et pas 4 ahahahha 2/view/footer.inc.php";
+    ?>
 </body>
 </html>
--- puissance 3 et pas 4 ahahahha 2/profil/index.php
+++ puissance 3 et pas 4 ahahahha 2/profil/index.php
@@ -7,18 +7,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <nav class="haut">
-        <div>
-        <p id="montitre">The Power Of Memory</p>
-        </div>
-        <div class="acc">
-        <a class="headerBtn" href="../accueil/index.php">ACCUEIL</a>
-            <a class="headerBtn" href="../inscription/index.php">S'INSCRIRE</a>
-            <a class="headerBtn" href="../login/index.php">SCORES</a>
-            <a class="headerBtn" href="../../puissance 3 et pas 4 ahahahha 2/contact/contact.php">CONTACTER</a>
-        </div>  
-    </nav>
-
+<?php
+    require "/Applications/MAMP/htdocs/github/puisssance4/puissance 3 et pas 4 ahahahha 2/view/header.inc.php";
+    ?>
     
     <div class="fond1 h1">
         <img class="proo" src="asset/end1.png" alt=""><p class="myman">PROFILE VERY COOL AND SWAG</p>
@@ -44,41 +35,8 @@
         </form>
 
     </section>
-
-    <section class="ses5">
-        <footer>
-            <div id="footerPart1">
-                <div id="info">
-                    <p>Information</p>
-                    <p>Quisque commodo facilisis purus, interdum volutpat arcu viverra sed</p>
-                    <p><span class="infoOrange">Tel : </span><span class="tex"> 05 04 03 02</p>
-                    <p><span class="infoOrange">Email :</span> support@powerofmemory.com</p>
-                    <p><span class="infoOrange">Location : </span>Paris</p>
-                    <div id="reseau">
-                        <div class="logoOrange"><img src="../puissance 3 et pas 4 ahahahha 2/asset/Google__G__Logo.svg.png" class="tailleImage"></img></div>
-                        <div class="logoOrange"><img src="../puissance 3 et pas 4 ahahahha 2/asset/twit.jpeg" class="tailleImage"></img></div>
-                        <div class="logoOrange"><img src="../puissance 3 et pas 4 ahahahha 2/asset/logo-facebook.png" class="tailleImage"></img></div>
-                        <div class="logoOrange"><img src="../puissance 3 et pas 4 ahahahha 2/asset/pint.png" class="tailleImage"></img></div>
-                        <div class="logoOrange"><img src="../puissance 3 et pas 4 ahahahha 2/asset/logo-instagram.png" class="tailleImage"></img></div>
-                    </div>
-                </div>
-    
-                <div id="power">
-                    <p>Power Of Memory</p>
-                    <p><span class="pointOrange">▪</span> <a class="loot" href="../login/index.php">Jouer ! </a> </p>
-                    <p><span class="pointOrange">▪</span> <a class="loot" href="../login/index.php">Les scores</a></p>
-                    <p><span class="pointOrange">▪</span> <a class="loot" href="../../puissance 3 et pas 4 ahahahha 2/contact/contact.php">Nous contacter </a></p>
-    
-                </div>
-            </div>
-    
-            <div id="footerPart2">
-                <p id="copyRight">Copyright © 2022 Tous droits réservés</p>
-            </div>
-        </footer>
-
-        <a class="gotopbtn" href="#"><i class="fa-solid fa-angle-up"></i></a>
-
-
+    <?php
+    require "/Applications/MAMP/htdocs/github/puisssance4/puissance 3 et pas 4 ahahahha 2/view/footer.inc.php";
+    ?>
 </body>
 </html>
