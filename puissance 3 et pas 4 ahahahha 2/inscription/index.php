38c569fc9b6e1e4cebd7aca37a4eb3f9e289e916
Sambachpx <samba.champroux@edu.esiee-it.fr>
2022-11-16 09:34:54 +0100

    redirection accueil

--- puissance 3 et pas 4 ahahahha 2/inscription/index.php
+++ puissance 3 et pas 4 ahahahha 2/inscription/index.php
@@ -12,7 +12,7 @@
         <p id="montitre">The Power Of Memory</p>
         </div>
         <div class="acc">
-        <a class="headerBtn" href="../puissance 3 et pas 4 ahahahha 2/index.php">ACCUEIL</a>
+        <a class="headerBtn" href="../accueil/index.php">ACCUEIL</a>
             <a class="headerBtn" href="../inscription/index.php">S'INSCRIRE</a>
             <a class="headerBtn" href="../login/index.php">SCORES</a>
             <a class="headerBtn" href="../../puissance 3 et pas 4 ahahahha 2/contact/contact.php">CONTACTER</a>
