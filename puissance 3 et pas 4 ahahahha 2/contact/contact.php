96548153c7d37e149595968af4a4299ce0986c6e
BaptisteGrimaldi <grimaldi.baptiste@gmail.com>
2022-11-16 09:20:46 +0100

    TEST

---
+++ puissance 3 et pas 4 ahahahha 2/contact/contact.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="contact.css">
+</head>
+<body>
+
+    
+
+    <nav>
+        <div id="nav1">
+            <p>The Power Of Memory</p>
+         </div>
+    
+         <div id="ajunav2">
+
+            <div id="nav2">
+                <a class="headerBtn" href="../accueil/index.html">ACCUEIL</a>
+                <a class="headerBtn" href="../inscription/index.html">S'INSCRIRE</a>
+                <a class="headerBtn" href="../login/index.html">SCORES</a>
+                <a class="headerBtn" href="../../puissance 3 et pas 4 ahahahha 2/contact/contact.html">CONTACTER</a>
+            </div>
+
+         </div>
+
+
+    </nav>
+
+
+    <header>
+        <div class="image1">
+        <p>NOUS CONTACTER</p>
+        </div>
+    </header>
+
+
+
+    <main>
+        <div id="icone">
+            <div class="tel">
+                <div class="reptel"><p>☏</p></div>
+                <p>06 05 04 03 02</p>
+            </div>
+            <div class="tel">
+                <div class="reptel"><p>📧</p></div>
+                <p id="suport">support@powerofmemory.com</p>
+            </div>
+            <div class="tel">
+                <div class="reptel"><p>📍</p></div>
+                <p>Paris</p>
+            </div>
+        </div>
+
+        <div id="formu">
+            <div id="nom">
+
+                <div id="divInputNom">
+                    <input id="inputNom"type="text" placeholder="Nom">
+                </div>
+                <div id="divInputEmail">
+                    <input id="inputMail"type="text"placeholder="Email">
+                </div>
+                
+                </div>
+                <div id="divInputSujet">
+                    <input id="inputSujet"type="text" placeholder="Sujet">
+                </div>
+            <div id="divInputMessage">
+                <input id="inputMessage"type="text" placeholder="Message">
+            </div>
+
+            <button id="envoyer">Envoyer</button>
+            
+        </div>
+    </main>
+
+    <section class="ses5">
+        <footer>
+            <div id="footerPart1">
+                <div id="info">
+                    <p>Information</p>
+                    <p>Quisque commodo facilisis purus, interdum volutpat arcu viverra sed</p>
+                    <p><span class="infoOrange">Tel : </span><span class="tex"> 05 04 03 02</p>
+                    <p><span class="infoOrange">Email :</span> support@powerofmemory.com</p>
+                    <p><span class="infoOrange">Location : </span>Paris</p>
+                    <div id="reseau">
+                        <div class="logoOrange"><img src="image/logofacebook.jpeg" class="tailleImage"></img></div>
+                        <div class="logoOrange"><img src="image/twit.jpeg" class="tailleImage"></img></div>
+                        <div class="logoOrange"><img src="image/Google-Logo.png" class="tailleImage"></img></div>
+                        <div class="logoOrange"><img src="image/pint.png" class="tailleImage"></img></div>
+                        <div class="logoOrange"><img src="image/Instagram_icon.png.webp" class="tailleImage"></img></div>
+                    </div>
+                </div>
+    
+                <div id="power">
+                    <p>Power Of Memory</p>
+                    <p><span class="pointOrange">▪</span> <a class="loot" href="../login/index.html">Jouer ! </a> </p>
+                    <p><span class="pointOrange">▪</span> <a class="loot" href="../login/index.html">Les scores</a></p>
+                    <p><span class="pointOrange">▪</span> <a class="loot" href="../../puissance 3 et pas 4 ahahahha 2/contact/contact.html">Nous contacter </a></p>
+    
+                </div>
+            </div>
+    
+            <div id="footerPart2">
+                <!-- <div id="flecheOrange">
+                    <p>^</p>
+                </div> -->
+                <p id="copyRight">Copyright © 2022 Tous droits réservés</p>
+            </div>
+        </footer>
+    </section>
+    <!-- <footer>
+        <div id="footerPart1">
+            <div id="info">
+                <p>Information</p>
+                <p>Quisque commodo facilisis purus, interdum volutpat arcu viverra sed</p>
+                <p><span class="infoOrange">Tel :</span>06 05 04 03 02</p>
+                <p><span class="infoOrange">Email :</span> support@powerofmemory.com</p>
+                <p><span class="infoOrange">Location :</span>Paris</p>
+                <div id="reseau">
+                    <div class="logoOrange"><img src="image/Le-logo-Facebook.jpg" class="tailleImage"></img></div>
+                    <div class="logoOrange"><img src="image/twitter-logo.jpg" class="tailleImage"></img></div>
+                    <div class="logoOrange"><img src="image/Google-Logo.png" class="tailleImage"></img></div>
+                    <div class="logoOrange"><img src="image/Logo-Pinterest-1.jpg" class="tailleImage"></img></div>
+                    <div class="logoOrange"><img src="image/Instagram_icon.png.webp" class="tailleImage"></img></div>
+                </div>
+            </div>
+    
+            <div id="power">
+                <p>Power Of Memory</p>
+                <p><span class="pointOrange">▪</span>  Jouer !</p>
+                <p><span class="pointOrange">▪</span>  Les scores</p>
+                <p><span class="pointOrange">▪</span>  Nous contacter</p>
+                
+            </div>
+        </div>
+        
+        <div id="footerPart2">
+            <div id="flecheOrange">
+                <p>^</p>
+            </div>
+            <p id="copyRight">Copyright © 2022 Tous droits réservés</p>
+        </div>
+    </footer> -->
+    <a class="gotopbtn" href="#"><i class="fa-solid fa-angle-up"></i></a>
+</body>
+</html>
