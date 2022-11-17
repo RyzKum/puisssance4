95d8238e5eb28aa3c60f2ab6730a64988a18caf5
doudouux <dounya.berramdane@edu.esiee-it.fr>
2022-11-17 09:56:24 +0100

    scores

---
+++ puissance 3 et pas 4 ahahahha 2/scores/scores.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Page de Connexion</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav class="haut">
+        <div>
+        <p id="montitre">The Power Of Memory</p>
+        </div>
+        <div class="acc">
+        <a class ="accueil" href="/puissance 3 et pas 4 ahahahha 2/puissance 3 et pas 4 ahahahha 2/index.html">ACCUEIL</a>
+        <a class ="jeu" href="">JEU</a>
+        <a class=" scores" href="">SCORES</a>
+        <a class="contact" href="/puissance 3 et pas 4 ahahahha 2/contact/contact.html">NOUS CONTACTER</a>
+        </div>
+    </nav>
+
+    <div class="fond1 h1">
+    <h1 class="h1">SCORES</h1>
+    </div>
+
+    <section class="formBlock">
+
+        <div class="top"> 
+            <div class="second">
+                <img src="pdp2.png" alt="">
+                <div class="sec">
+                <p>2eme <br></p>
+                <p>Potaaato</p>
+                <p>1:51.712</p>
+            </div>
+            </div>
+            <div class="first">
+                <img src="pdp1.jpg" alt="">
+                <div class="fir">
+                <p>1er<br></p>
+                <p>Chønic </p>
+                <p>1:50.500</p>
+            </div>
+            </div>
+            <div class="third">
+                <img src="pdp3.png" alt="">
+                <div class="th">
+                <p>3eme <br></p>
+                <p>Az0Xx</p>
+                <p>1:70.000</p>
+            </div>
+            </div>
+        </div>
+
+    <div class="ranking">
+        <div class="position"><p>4eme</p><img src="..\scores\assetpdp4.jfif" alt=""><p>KendiX</p><p>2:00.310</p></div>
+        <div class="position"><p>5eme</p><img src="..\asset\pdp5.png" alt=""><p>Xeno</p><p>2:05.850</p></div>
+        <div class="position"><p>6eme</p><img src="pdp6.png" alt=""><p>SL1D3R</p><p>2:08.610</p></div>
+        <div class="position"><p>7eme</p><img src="pdp7.png" alt=""><p>TroniiX</p><p>2:15.420</p></div>
+        <div class="position"><p>8eme</p><img src="pdp8.png" alt=""><p>AKUMA</p><p>2:17.050</p></div>
+        <div class="position"><p>9eme</p><img src="pdp9.jpg" alt=""><p>zReeZ_</p><p>2:50.573</p></div>
+    </div>
+
+    </section>
+
+    <section class="ses5">
+        <footer>
+            <div id="footerPart1">
+                <div id="info">
+                    <p>Information</p>
+                    <p>Quisque commodo facilisis purus, interdum volutpat arcu viverra sed</p>
+                    <p><span class="infoOrange">Tel : </span><span class="tex"> 05 04 03 02</p>
+                    <p><span class="infoOrange">Email :</span> support@powerofmemory.com</p>
+                    <p><span class="infoOrange">Location : </span>Paris</p>
+                    <div id="reseau">
+                        <div class="logoOrange"><img src="/puissance 3 et pas 4 ahahahha 2/puissance 3 et pas 4 ahahahha 2/asset/logofacebook.jpeg" class="tailleImage"></img></div>
+                        <div class="logoOrange"><img src="/puissance 3 et pas 4 ahahahha 2/puissance 3 et pas 4 ahahahha 2/asset/twit.jpeg" class="tailleImage"></img></div>
+                        <div class="logoOrange"><img src="/puissance 3 et pas 4 ahahahha 2/puissance 3 et pas 4 ahahahha 2/asset/Google-Logo.png" class="tailleImage"></img></div>
+                        <div class="logoOrange"><img src="/puissance 3 et pas 4 ahahahha 2/puissance 3 et pas 4 ahahahha 2/asset/pint.png" class="tailleImage"></img></div>
+                        <div class="logoOrange"><img src="/puissance 3 et pas 4 ahahahha 2/puissance 3 et pas 4 ahahahha 2/asset/Instagram_icon.png.webp" class="tailleImage"></img></div>
+                    </div>
+                </div>
+    
+                <div id="power">
+                    <p>Power Of Memory</p>
+                    <p><span class="pointOrange">▪</span>  Jouer !</p>
+                    <p><span class="pointOrange">▪</span>  Les scores</p>
+                    <p><span class="pointOrange">▪</span>  Nous contacter</p>
+    
+                </div>
+            </div>
+    
+            <div id="footerPart2">
+                <p id="copyRight">Copyright © 2022 Tous droits réservés</p>
+            </div>
+        </footer>
+
+
+</body>
+</html>
