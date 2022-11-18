0c74c9db9e804b89455adfe954eeee8ca5008a13
Sambachpx <samba.champroux@edu.esiee-it.fr>
2022-11-18 11:55:21 +0100

    rajout lien header

--- puissance 3 et pas 4 ahahahha 2/view/header.inc.php
+++ puissance 3 et pas 4 ahahahha 2/view/header.inc.php
@@ -12,10 +12,9 @@
         <div class="acc">
         <a class="headerBtn" href="../accueil/index.php">ACCUEIL</a>
             <a class="headerBtn" href="../inscription/index.php">S'INSCRIRE</a>
-            <a class="headerBtn" href="../login/index.php">SCORES</a>
+            <a class="headerBtn" href="../login/index.php">SE CONNECTER</a>
             <a class="headerBtn" href="../contact/contact.php">CONTACTER</a>
             <a class="headerBtn" href="../scores/scores.php">SCORES</a>
-            <a class="headerBtn" href="../login/in.php">SE CONNECTER</a>
             
         </div>  
  </nav>
