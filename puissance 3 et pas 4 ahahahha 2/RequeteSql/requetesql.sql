654ab940282f806df28a30615b2260ef45c9cb10
Sambachpx <samba.champroux@edu.esiee-it.fr>
2022-11-18 14:49:44 +0100

    date et heure message

--- puissance 3 et pas 4 ahahahha 2/RequeteSql/requetesql.sql
+++ puissance 3 et pas 4 ahahahha 2/RequeteSql/requetesql.sql
@@ -23,7 +23,7 @@ INSERT INTO `jeu` (`id`, `nom_jeu`) VALUES ('4', 'The Tower of memory');
 SELECT * FROM `utlisateur` WHERE `email` = 'baptiste@gmail.com' AND `mot_de_passe` = 'ptdrlo';
 
 /* requette message 24h (pas finis)*/
-SELECT * FROM `message(tchat)` WHERE date_heure_mess 
+SELECT * FROM `message(tchat)` WHERE date_heure_mess < 24; 
 
 /* trier du plus récent au plus vieux */
 SELECT * FROM `message(tchat)` ORDER BY `date_heure_mess` DESC
