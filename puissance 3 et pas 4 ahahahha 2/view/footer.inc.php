1668ac099a07d45d510cbadb90be46b7ac3cc382
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2022-11-25 11:42:24 +0100

    correction footer image

--- puissance 3 et pas 4 ahahahha 2/view/footer.inc.php
+++ puissance 3 et pas 4 ahahahha 2/view/footer.inc.php
@@ -18,12 +18,12 @@
                     <p><span class="infoOrange">Email :</span> support@powerofmemory.com</p>
                     <p><span class="infoOrange">Location : </span>Paris</p>
                     <div id="reseau">
-                        <div class="logoOrange"><img src="/asset/logo-instagram.png" class="tailleImage"></img></div>
-                        <div class="logoOrange"><img src="asset/twit.jpeg" class="tailleImage"></img></div>
-                        <div class="logoOrange"><img src="asset/Google__G__Logo.svg.png" class="tailleImage"></img>
+                        <div class="logoOrange"><img src="../asset/logo-instagram.png" class="tailleImage"></img></div>
+                        <div class="logoOrange"><img src="../asset/twit.jpeg" class="tailleImage"></img></div>
+                        <div class="logoOrange"><img src="../asset/Google__G__Logo.svg.png" class="tailleImage"></img>
                         </div>
-                        <div class="logoOrange"><img src="asset/pint.png" class="tailleImage"></img></div>
-                        <div class="logoOrange"><img src="asset/Instagram_icon.png.webp" class="tailleImage"></img>
+                        <div class="logoOrange"><img src="../asset/pint.png" class="tailleImage"></img></div>
+                        <div class="logoOrange"><img src="../asset/Instagram_icon.png.webp" class="tailleImage"></img>
                         </div>
                     </div>
                 </div>
