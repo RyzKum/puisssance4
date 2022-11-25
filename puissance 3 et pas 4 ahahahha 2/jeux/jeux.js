9a4609cb3c41d9ab31829f05c71ecec643a29c9d
RyzKu <103863031+RyzKum@users.noreply.github.com>
2022-11-25 10:42:06 +0100

    chrono finish

--- puissance 3 et pas 4 ahahahha 2/jeux/jeux.js
+++ puissance 3 et pas 4 ahahahha 2/jeux/jeux.js
@@ -266,3 +266,49 @@ memory.addEventListener("click",(e)=>{
 
 })
 
+
+var startTime = 0
+var start = 0
+var end = 0
+var diff = 0
+var timerID = 0
+function chrono(){
+	end = new Date()
+	diff = end - start
+	diff = new Date(diff)
+	var msec = diff.getMilliseconds()
+	var sec = diff.getSeconds()
+	var min = diff.getMinutes()
+	if (min < 10){
+		min = "0" + min
+	}
+	if (sec < 10){
+		sec = "0" + sec
+	}
+	if(msec < 10){
+		msec = "00" +msec
+	}
+	else if(msec < 100){
+		msec = "0" +msec
+	}
+	document.getElementById("chronotime").innerHTML = min + ":" + sec + ":" + msec
+	timerID = setTimeout("chrono()", 10)
+}
+function chronoStart(){
+	document.chronoForm.startstop.value = "stop!"
+	document.chronoForm.startstop.onclick = chronoStop
+	start = new Date()
+	chrono()
+}
+function chronoContinue(){
+	document.chronoForm.startstop.value = "stop!"
+	document.chronoForm.startstop.onclick = chronoStop
+	start = new Date()-diff
+	start = new Date(start)
+	chrono()
+}
+function chronoStop(){
+	document.chronoForm.startstop.value = "start!"
+	document.chronoForm.startstop.onclick = chronoContinue
+	clearTimeout(timerID)
+}
