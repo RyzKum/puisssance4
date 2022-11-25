5c6402c4605a3197f4a7cb30042df71d858b3018
BaptisteGrimaldi <grimaldi.baptiste@gmail.com>
2022-11-25 11:06:04 +0100

    test

--- puissance 3 et pas 4 ahahahha 2/jeux/jeux.js
+++ puissance 3 et pas 4 ahahahha 2/jeux/jeux.js
@@ -41,17 +41,17 @@ selectTheme.addEventListener("mouseenter",()=>{
 
 
 
-outSelect.addEventListener("mouseleave",()=>{
+// outSelect.addEventListener("mouseleave",()=>{
 
 
-    document.getElementsByClassName("injectStyle")[0].remove();
-    document.getElementsByClassName("injectStyle")[0].remove();
-    document.getElementsByClassName("injectStyle")[0].remove();
-    contain.style.height = "30px";
-    let containLevel = document.getElementById("jeuxGlobal");
-    containLevel.style.height = "30px";
+//     document.getElementsByClassName("injectStyle")[0].remove();
+//     document.getElementsByClassName("injectStyle")[0].remove();
+//     document.getElementsByClassName("injectStyle")[0].remove();
+//     contain.style.height = "30px";
+//     let containLevel = document.getElementById("jeuxGlobal");
+//     containLevel.style.height = "100px";
 
-})
+// })
 
 
 // Defilement level
@@ -99,7 +99,7 @@ contain.addEventListener("mouseleave",()=>{
     document.getElementsByClassName("injectStyle")[0].remove();
     document.getElementsByClassName("injectStyle")[0].remove();
     document.getElementsByClassName("injectStyle")[0].remove();
-    contain.style.height = "25px";
+    contain.style.height = "40px";
     
 })
 
